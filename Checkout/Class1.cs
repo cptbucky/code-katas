818005ee0f4923fb65d0eed4027a92723aa9e55a
cptbucky <cpt.bucky@gmail.com>
2013-11-16 17:40:43 +0000

    scan single a

--- Checkout/Class1.cs
+++ Checkout/Class1.cs
@@ -5,5 +5,23 @@ namespace Checkout
     [TestFixture]
     public class Class1
     {
+        [Test]
+        public void get_price_of_single_A()
+        {
+            // arrange
+            var expected = 50;
+            var item = 'A';
+
+            // act
+            var actual = Scan(item);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        private object Scan(char item)
+        {
+            return 50;
+        }
     }
 }
