67a17ff644a1d46479e73ba70d869883bef851c7
cptbucky <cpt.bucky@gmail.com>
2013-11-16 19:44:43 +0000

    refactored components into own files

---
+++ Checkout/Checkout.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Checkout
+{
+    public interface ICheckout
+    {
+        int Total { get; }
+
+        void Scan(char sku);
+
+        void Scan(char[] skus);
+    }
+
+    public class Checkout : ICheckout
+    {
+        public int Total {
+            get { return GetBasketTotal(); }
+        }
+
+        private readonly List<char> _scannedSkus;
+        private readonly IPricingRules _pricingRules;
+
+        public Checkout(IPricingRules pricingRules)
+        {
+            _scannedSkus = new List<char>();
+            _pricingRules = pricingRules;
+        }
+
+        public void Scan(char sku)
+        {
+            _scannedSkus.Add(sku);
+        }
+
+        public void Scan(char[] skus)
+        {
+            _scannedSkus.AddRange(skus);
+        }
+
+        public int GetBasketTotal()
+        {
+            if (_scannedSkus.Count == 2 && _scannedSkus.All(x => x == 'B'))
+            {
+                return _scannedSkus.Sum(x => _pricingRules.GetSkuPrice(x)) - 15;
+            }
+
+            if (_scannedSkus.Count == 3 && _scannedSkus.All(x => x == 'A'))
+            {
+                return _scannedSkus.Sum(x => _pricingRules.GetSkuPrice(x)) - 20;
+            }
+
+            return _scannedSkus.Sum(x => _pricingRules.GetSkuPrice(x));
+        }
+    }
+}
---
+++ Checkout/PricingRules.cs
@@ -0,0 +1,30 @@
+﻿namespace Checkout
+{
+    public interface IPricingRules
+    {
+        int GetSkuPrice(char sku);
+    }
+
+    public class PricingRules : IPricingRules
+    {
+        public int GetSkuPrice(char sku)
+        {
+            if (sku == 'D')
+            {
+                return 15;
+            }
+
+            if (sku == 'C')
+            {
+                return 20;
+            }
+
+            if (sku == 'B')
+            {
+                return 30;
+            }
+
+            return 50;
+        }
+    }
+}
