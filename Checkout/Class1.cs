ece2a75c25944c9a7e63615e2e14c6aca51baf10
cptbucky <cpt.bucky@gmail.com>
2013-11-16 17:54:39 +0000

    scan single b

--- Checkout/Class1.cs
+++ Checkout/Class1.cs
@@ -6,7 +6,7 @@ namespace Checkout
     public class Class1
     {
         [Test]
-        public void get_price_of_single_A()
+        public void get_price_of_single_A_should_equal_50()
         {
             // arrange
             var expected = 50;
@@ -19,8 +19,27 @@ namespace Checkout
             Assert.AreEqual(expected, actual);
         }
 
+        [Test]
+        public void get_price_of_single_B_should_equal_30()
+        {
+            // arrange
+            var expected = 30;
+            var item = 'B';
+
+            // act
+            var actual = Scan(item);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
         private object Scan(char item)
         {
+            if (item == 'B')
+            {
+                return 30;
+            }
+
             return 50;
         }
     }
