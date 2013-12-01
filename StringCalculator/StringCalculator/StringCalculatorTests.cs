ea15df9a056b68bb7d6a6faa2975b2e1403cad03
cptbucky <cpt.bucky@gmail.com>
2013-12-01 16:54:20 +0000

    Step 2+3: unknown amount of numbers, handle newlines

--- StringCalculator/StringCalculator/StringCalculatorTests.cs
+++ StringCalculator/StringCalculator/StringCalculatorTests.cs
@@ -36,6 +36,42 @@ namespace StringCalculator
             Assert.AreEqual(expected, actual);
         }
 
+        [Test]
+        public void add_seven_numbers_should_return_sum()
+        {
+            int expected = 25;
+            int actual = Add("1,2,3,4,4,5,6");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void add_four_numbers_should_return_sum()
+        {
+            int expected = 14;
+            int actual = Add("1,2,5,6");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void add_two_numbers_delimeted_by_newline_should_return_sum()
+        {
+            int expected = 3;
+            int actual = Add("1\n2");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void add_two_numbers_delimeted_by_newline_and_comma_should_return_sum()
+        {
+            int expected = 4;
+            int actual = Add("1\n2,1");
+
+            Assert.AreEqual(expected, actual);
+        }
+
         private int Add(string numbers)
         {
             if (numbers == string.Empty)
@@ -43,16 +79,21 @@ namespace StringCalculator
                 return 0;
             }
 
-            int[] singles = numbers.Split(',').Select(x => Convert.ToInt32(x)).ToArray();
+            var singles = GetIntArray(numbers);
 
-            int sum = 0;
+            return singles.Sum();
+        }
 
-            for (int i = 0; i < singles.Length; i++)
-            {
-                sum += singles[i];
-            }
+        private static int[] GetIntArray(string numbers)
+        {
+            var delimeter = '|';
 
-            return sum;
+            var singleDelimeted = numbers.Replace('\n', delimeter).Replace(',', delimeter);
+
+            //var delimeter = numbers.Contains("\n") ? '\n' : ',';
+
+            int[] singles = singleDelimeted.Split(delimeter).Select(x => Convert.ToInt32(x)).ToArray();
+            return singles;
         }
     }
 }
